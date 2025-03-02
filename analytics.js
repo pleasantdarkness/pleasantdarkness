6a80a35e3d9ad2ec382e86af3c6fd82f6806edd7
pleasantdarkness <pdwriterbhumi@gmail.com>
2025-03-02 11:36:56 +0530

    Add files via upload

---
+++ analytics.js
@@ -0,0 +1,11 @@
+(function() {
+    var script = document.createElement('script');
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-16KGM78CSP";
+    script.async = true;
+    document.head.appendChild(script);
+
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-16KGM78CSP');
+})();
